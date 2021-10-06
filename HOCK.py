06af0b8c8d61459b812aba78eeac13f84514455f
ChronosNoob <83444922+ChronosNoob@users.noreply.github.com>
2021-10-06 21:17:20 +0100

    Updated HOCK.py
    Made to have single run

--- HOCK.py
+++ HOCK.py
@@ -1,5 +1,23 @@
 import time
 import random
 for i in range(1,101,1):
-    print("Disemboweling Chidren " + str(i) + "%")
-    time.sleep(random.randfloat(0,2))
+    print("Disemboweling Chidren... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Installing Java Runtime Components... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Recalibrating Gyroscopic Sensors... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Reconstruction of the Human DNA... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Hacking... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Entering the mainframe... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Proceeding with inception... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Taking the blue pill... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Taking the red pill... " + str(i) + "%: " + str(i) + "/100GB")
+for i in range(1,101,1):
+    print("Infecting Your Computer... " + str(i) + "%: " + str(i) + "/100GB")
+print("DONE , YOUR SYS ADMIN HAS NOW BEEN COMPRIMISED BY HAXXOR :)")
